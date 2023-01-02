470bb07b793d96fb59ce8ff1bfe12fca4f6d30d5
bombastion <tyler.tift@gmail.com?>
2023-01-01 21:45:21 -0700

    Tests on items

--- src/db.py
+++ src/db.py
@@ -12,6 +12,12 @@ class InventoryDB(ABC):
         pass
 
     @abstractmethod
+    def get_item(self, id: int) -> Item:
+        """ Finds an item by ID
+        """
+        pass
+
+    @abstractmethod
     def add_item_metadata(self, metadata: ItemMetadata) -> ItemMetadata:
         """ Adds an item metaedata to the DB and hydrates its ID
         """
@@ -33,17 +39,20 @@ class InMemoryDB(InventoryDB):
         if item.metadata_id is None or item.metadata_id not in self.item_metadata.keys():
             raise ValueError(f"Invalid metadata_id {item.metadata_id}")
 
+    def _add_db_item(self, object: typing.Any, validate_method: typing.Callable, collection: typing.Dict[int, typing.Any]) -> typing.Any:
+        db_object = deepcopy(object)
+        validate_method(db_object)
+        if db_object.id is None:
+            db_object.id = self._get_next_id(collection.keys())
+        
+        collection[db_object.id] = db_object
+        return db_object
+
     def add_item(self, item: Item) -> Item:
         """ Adds an item to the DB and hydrates its ID if empty
         returns: A deep copy of the updated item
         """
-        db_item = deepcopy(item)
-        self._validate_item(db_item)
-        if db_item.id is None:
-            db_item.id = self._get_next_id(self.items.keys())
-
-        self.items[db_item.id] = db_item
-        return db_item
+        return self._add_db_item(item, self._validate_item, self.items)
 
     def _validate_item_metadata(self, metadata: ItemMetadata)-> None:
         if not metadata.name:
@@ -53,10 +62,4 @@ class InMemoryDB(InventoryDB):
         """ Adds an item metadata to the DB and hydrates its ID if empty
         returns: A deep copy of the updated metadata
         """
-        db_object = deepcopy(metadata)
-        self._validate_item_metadata(db_object)
-        if db_object.id is None:
-            db_object.id = self._get_next_id(self.item_metadata.keys())
-
-        self.item_metadata[db_object.id] = db_object
-        return db_object
+        return self._add_db_item(metadata, self._validate_item_metadata, self.item_metadata)
