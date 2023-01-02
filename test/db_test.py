d45e2b539992c3fa09acb613055b9cc7c63d28c5
bombastion <tyler.tift@gmail.com?>
2023-01-01 21:22:25 -0700

    First unit test

--- test/db_test.py
+++ test/db_test.py
@@ -20,3 +20,18 @@ def test_add_item_metadata():
 
     assert result.id == 0
     assert result.name == "test"
+    assert len(subject.item_metadata) == 1
+
+
+def test_add_item_metadata__increments_id():
+    test_item = ItemMetadata(name="test")
+
+    subject = InMemoryDB()
+
+    subject.add_item_metadata(test_item)
+    # Add a second entry to test IDs
+    result = subject.add_item_metadata(test_item)
+
+    assert result.id == 1
+    assert result.name == "test"
+    assert len(subject.item_metadata) == 2
